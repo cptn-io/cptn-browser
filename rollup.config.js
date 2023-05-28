1c59ee8df349259bafe2153ac0aaf76e39e4e1c3
KC <kcthota@gmail.com>
2023-05-28 00:24:32 -0700

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/index.js',
+        format: 'umd',
+        name: 'index'
+    },
+    plugins: [
+        resolve({
+            browser: true
+        })
+    ]
+};
